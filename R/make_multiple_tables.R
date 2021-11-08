fcc0262dd3247d3e047bdb5a935e25d616f19950
Shiloh Bradley <shilohfling@gmail.com>
2021-11-08 11:08:28 -0800

    forgot importFrom

--- R/make_multiple_tables.R
+++ R/make_multiple_tables.R
@@ -3,16 +3,17 @@
 #' @param df a very long table
 #' @param row_length the amount of rows per each table
 #'
+#' @importFrom xtable xtable
 #' @return
 #' @export
 #'
 #' @examples
-#' 
+#'
 
 make_multiple_tables <- function(df, row_length = 30) {
-  
+
   for (i in 1:ceiling(length(names(df)) / row_length)) {
-    
+
     dt <- data.frame(x = seq((i - 1) * row_length + 1, min(c(length(names(df)), i * row_length)), 1),
                      names = names(df[((i - 1) * row_length + 1):(min(c(length(names(df)), i * row_length)))]))
 
