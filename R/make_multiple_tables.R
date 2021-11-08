7acd155577a6f59b4a3130d144209ccdc34a824b
Shiloh Bradley <shilohfling@gmail.com>
2021-11-08 11:01:25 -0800

    commit first function to break up big tables

---
+++ R/make_multiple_tables.R
@@ -0,0 +1,26 @@
+#' Make multiple short tables out of one long table
+#'
+#' @param df a very long table
+#' @param row_length the amount of rows per each table
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' 
+
+make_multiple_tables <- function(df, row_length = 30) {
+  
+  for (i in 1:ceiling(length(names(df)) / row_length)) {
+    
+    dt <- data.frame(x = seq((i - 1) * row_length + 1, min(c(length(names(df)), i * row_length)), 1),
+                     names = names(df[((i - 1) * row_length + 1):(min(c(length(names(df)), i * row_length)))]))
+
+    print(xtable::xtable(dt,
+                         caption = paste0("Part ", i)),
+                  caption.placement = "top",
+                  include.rownames = FALSE,
+                  include.colnames = TRUE,
+                  comment = FALSE)
+  }
+}
